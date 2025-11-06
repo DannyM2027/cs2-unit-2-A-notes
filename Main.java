236dd79728d2908eb13db6e450afaf385d8b335e
DannyM2027 <daniel.matut2027@gbwl.org>
2025-11-06 19:22:41 +0000

    2.4

--- Main.java
+++ Main.java
@@ -30,6 +30,50 @@ public class Main {
       System.out.println( x == z);
 
 
+      // IF STATEMENTS: use boolean expressions to make DECISIONS about which code to execute 
+      double time = 8.5; // means 8:30 AM 
+      time = 7.75; // means 7:45 AM 
+      time = 8.25; // means 8:15 AM 
+      // Two way SELECTION: if block plus + else block
+      time = 8.0;
+      if (time < 8) {
+         System.out.println("Eligible for elevator!"); 
+      }
+      else {
+         // Statement in ELSE block runs when IF block is false 
+         System.out.println("Take the stairs..."); 
+      }
+
+      // Another example of TWO-WAY SELECTION 
+      // in this case, using just a boolean VALUE (not an EXPRESSION) 
+      boolean heads = false; // flipped tails  
+      // if (true)
+      if (heads == true) {
+         System.out.println("Flipped Heads! Wear White."); 
+      }
+      else {
+         System.out.println("Flipped tails! Wear black.");
+      }
+
+      // Multi way selectiobn
+      // 1. NESTING if statments is loke asking follow up questions
+      int age = 6;
+      boolean havePermit = false;
+      System.out.println("can u drive");
+
+      if ( age >= 16 ) {
+         System.out.println("I GUESSS r old enough");
+         // pass first, ask follow
+         if (havePermit == true) {
+            System.out.println("get in scallywag");
+         }
+      }
+      else {
+         System.out.println("noo ur young boi get ur age up");
+      }
+      
+
+
 
 
 
